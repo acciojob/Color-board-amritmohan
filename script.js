7ddb48fa0fee5140daf75f94e45d03ae30bd54d9
@amritmohan <amritmohan2284@gmail.com>
2025-02-18 01:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//your JS code here. If required.
+// //your JS code here. If required.
 
-const container = document.querySelector('.container');
-    for (let i = 0; i < 800; i++) {
-      const square = document.createElement('div');
-      square.classList.add('square');
-      container.appendChild(square);
-    }
+// const container = document.querySelector('.container');
+//     for (let i = 0; i < 800; i++) {
+//       const square = document.createElement('div');
+//       square.classList.add('square');
+//       container.appendChild(square);
+//     }
